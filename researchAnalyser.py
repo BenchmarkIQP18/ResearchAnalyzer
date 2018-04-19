fa089696e48cf737cad8c9a9e5e988e8091f5635
iwwoods <isaac.woods.official@gmail.com>
2018-04-19 15:55:09 +0100

    Add gitignore to ignore .tsv/.?sv, remove commented stuff

--- researchAnalyser.py
+++ researchAnalyser.py
@@ -3,7 +3,6 @@ import sys
 import json
 import os
 import re
-# import tsv
 
 # Setup keyword regex
 keywordList = ["\\bAccess to Justice","Accountable Institutions","Affordable Energy","All ages--elderly","Biodiviersity","Cities","Climate Change","Conserve Oceans","Consumption","Decent Work","Desertification","Economic Growth","Ecosystems","Employment","Empower women","Energy","Equitable Education","Food Security","Foster Innovation","Gender Equality","Girls","Global Partnership for Sustainable Development","Healthy Lives","Human Settlements","Hunger","Inclusive Cities","Inclusive Education","Inclusive Human Settlements","Inclusive Institutions","Inclusive Societies","Industrialization","Inequality","Infrastructure","Innovation","Justice","Land Degradation","Land","Manage Forests","Marine","Nutrition","Oceans","Opportunities for all","Peaceful Societies","Poverty","Productive Employment","Productive Patterns","Reduce Inequality","Reliable Energy","Resilient Infrastructure","Sanitation","Seas","Sustainability","Sustainable Agriculture","Sustainable Consumption","Sustainable Economic Growth","Sustainable Energy","Sustainable Growth","Sustainable Industrialization","Sustainable Oceans","Sustainable","Terrestrial Ecosystems","Water","Well-Being","Women\\b"]
@@ -79,11 +78,6 @@ atsv = open("susAuthors.tsv", 'w', encoding="utf-8")
 # Write authors
 atsv.write('\n'.join(susAuthors))
 atsv.close()
-
-# # And the same for all authors
-# aatsv = open("allAuthors.tsv", 'w', encoding="utf-8")
-# aatsv.write('\n'.join(allAuthors))
-# aatsv.close()
 
 # Print Results
 print("Done")
